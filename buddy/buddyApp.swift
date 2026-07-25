33b2499c4bd376c0b2eeb444611075002b3ed7fe
VarunCS1034 <vkeshett@gitam.in>
2026-07-25 17:33:10 +0530

    Initial Commit

---
+++ buddy/buddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  buddyApp.swift
+//  buddy
+//
+//  Created by Varun Keshetti on 25/07/26.
+//
+
+import SwiftUI
+
+@main
+struct buddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ buddyTests/buddyTests.swift
@@ -0,0 +1,18 @@
+//
+//  buddyTests.swift
+//  buddyTests
+//
+//  Created by Varun Keshetti on 25/07/26.
+//
+
+import Testing
+
+struct buddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
